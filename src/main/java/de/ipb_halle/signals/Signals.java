3bdede3d25a00d6ec68bdcefd86b2deb2f1a7d24
Frank Broda <fbroda@ipb-halle.de>
2022-09-30 09:03:44 +0200

    add commandline processing

--- src/main/java/de/ipb_halle/signals/Signals.java
+++ src/main/java/de/ipb_halle/signals/Signals.java
@@ -29,8 +29,21 @@ import javax.ejb.embeddable.EJBContainer;
 import javax.inject.Inject;
 import javax.naming.Context;
 import javax.naming.InitialContext;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.MissingArgumentException;
+import org.apache.commons.cli.MissingOptionException;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.UnrecognizedOptionException;
 import org.apache.openejb.OpenEjbContainer;
 import org.apache.openejb.api.LocalClient;
+
+
 
 /** 
  * IPB Signals client is a tool for data import and export
@@ -48,6 +61,29 @@ public class Signals {
 
     @Inject
     private LdapClient ldapClient;
+
+    @SuppressWarnings("static-access")
+    private static final Option configOpt = Option.builder("c")
+      .longOpt("config")
+      .desc("Path to a the config file containing the Signals (tm) web token, base URL as well as database and LDAP connection information.")
+      .hasArg()
+      .argName("FILE")
+      .required(true)
+      .build();
+
+    @SuppressWarnings("static-access")
+    private static final Option helpOpt = Option.builder("h")
+      .longOpt("help")
+      .desc("Display the help")
+      .build();
+
+    @SuppressWarnings("static-access")
+    private static final Option userMgrOpt = Option.builder("u")
+    .longOpt("manage-users")
+    .desc("Perform user,  group and role management")
+    .build();
+
+
 
     public void doIt() {
 
@@ -85,7 +121,12 @@ public class Signals {
         }
     }
 
-    public static void runClient(String fname) {
+    private void manageUsers() {
+        System.out.println("Managing users ...");
+    }
+
+    public static Signals getInstance(String fname) {
+        Signals signals = null;
         try {
 
             Properties properties = new Properties();
@@ -95,22 +136,76 @@ public class Signals {
             EJBContainer container = EJBContainer.createEJBContainer(properties);
             Context ctx = container.getContext();
 
-            Signals signals = new Signals();
+            signals = new Signals();
             ctx.bind("inject", signals);
 
-            signals.doIt();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return signals;
+    }
 
+    public static void printHelp(String errorMessage, Options options) {
+        HelpFormatter writer = new HelpFormatter();
+        String usage = "java -jar <JAR_WITH_DEPENDENCIES>";
+        String header = "\n\nIPB Signals Tool\n(c) 2022 Leibniz Institute of Plant Biochemistry\n\nManagement tool for your PerkinElmer(r) Signals(tm) Notebook instance.\n\nOPTIONS:\n";
+        StringBuilder sb = new StringBuilder();
+        if (errorMessage != null) {
+            sb.append("\n");
+            sb.append(errorMessage);
+            sb.append("\n");
+        }
+        String footer = sb.toString();
+        writer.printHelp(usage, header, options, footer, true);
+    }
+
+    /**
+     * process the command line and perform requested jobs. 
+     * @param argv the command line arguments
+     * @param options the defined options
+     */
+    public static void processCommandLine(String[] argv, Options options) {
+
+        CommandLineParser parser = new DefaultParser();
+        try {
+            CommandLine cmdline = parser.parse(options, argv);
+
+            if (cmdline.hasOption(helpOpt.getOpt())) {
+                printHelp(null, options);
+                return;
+            }
+
+            if (! cmdline.hasOption(configOpt.getOpt())) {
+                printHelp("ERROR: missing configuration file", options);
+                return;
+            }
+            String configFile = cmdline.getOptionValue(configOpt.getOpt());
+            Signals signals = getInstance(configFile);
+
+
+            if (cmdline.hasOption(userMgrOpt.getOpt())) {
+                signals.manageUsers();
+            } 
+
+        } catch(MissingArgumentException mae) {
+            printHelp("ERROR: " + mae.getMessage(), options);
+        } catch(MissingOptionException moe) {
+            printHelp("ERROR: " + moe.getMessage(), options);
+        } catch(UnrecognizedOptionException uoe) {
+            printHelp("ERROR: " + uoe.getMessage(), options);
         } catch(Exception e) {
             e.printStackTrace();
         }
     }
 
+
     public static void main(String[] argv) {
-        if (argv.length != 1) {
-            System.out.println("Usage: java -jar signals-with-dependencies.jar CONFIGFILE");
-            return;
-        } 
-        runClient(argv[0]);
+        Options options = new Options();
+        options.addOption(configOpt);
+        options.addOption(helpOpt);
+        options.addOption(userMgrOpt);
+
+        processCommandLine(argv, options);
     }
 }
 
