b8dbe8b2af0f841248236febc22964b2055028df
Wittchen <swittche@ipb-halle.de>
2025-08-08 11:19:10 +0200

    Java Docs adde to ada processor bean

--- src/main/java/de/ipb_halle/signals/ado/AdoProcessorBean.java
+++ src/main/java/de/ipb_halle/signals/ado/AdoProcessorBean.java
@@ -1,7 +1,7 @@
 /*
  *
  *  * IPB Signals client
- *  * Copyright 2024 Leibniz-Institut f. Pflanzenbiochemie
+ *  * Copyright 2025 Leibniz-Institut f. Pflanzenbiochemie
  *  *
  *  * Licensed under the Apache License, Version 2.0 (the "License");
  *  * you may not use this file except in compliance with the License.
@@ -33,27 +33,74 @@ import jakarta.transaction.TransactionSynchronizationRegistry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.Logger;
 
+/**
+ * Stateless EJB for processing single Ado entities.
+ * <p>
+ * This bean is responsible for retrieving an Ado from the remote Signals REST API,
+ * logging its properties and property values, fetching its template fields if available,
+ * and persisting it to the database. The processing runs within its own transaction
+ * to ensure database operations are committed independently.
+ * </p>
+ *
+ * <p>Main responsibilities:</p>
+ * <ul>
+ *     <li>Retrieve an Ado object by its EID via {@link AdoRestService}</li>
+ *     <li>Fetch additional template fields if a template ID exists</li>
+ *     <li>Log Ado properties and property values for debugging purposes</li>
+ *     <li>Persist the processed Ado object using {@link AdoDbService}</li>
+ * </ul>
+ *
+ * <p>
+ * The class uses {@link TransactionSynchronizationRegistry} to check the transaction status
+ * and skip processing if the transaction is marked for rollback.
+ * </p>
+ *
+ * @author swittche
+ * @version 1.0
+ * @since 2025
+ */
 @Stateless
 @LocalBean
 public class AdoProcessorBean {
 
+    private static final Logger logger = (Logger) LogManager.getLogger(AdoProcessorBean.class);
     @Inject
     AdoRestService adoRestService;
-
     @Inject
     AdoDbService adoDbService;
-
     @Resource
     TransactionSynchronizationRegistry synchronizationRegistry;
 
-    private static final Logger logger = (Logger) LogManager.getLogger(AdoProcessorBean.class);
-
-
+    /**
+     * Processes a single Ado object identified by its EID within a new transaction.
+     * <p>
+     * This method ensures that the retrieval, logging, and persistence of the Ado
+     * are executed in an isolated transactional context.
+     * </p>
+     *
+     * @param eid The entity identifier (EID) of the Ado to process.
+     */
     @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
     public void processSingleAdo(String eid) {
         doProcessAdo(eid);
     }
 
+    /**
+     * Internal helper method that performs the actual processing of the Ado.
+     * <p>
+     * Steps performed:
+     * <ol>
+     *     <li>Check if the current transaction is marked for rollback; if so, skip processing.</li>
+     *     <li>Retrieve the Ado from the REST service using {@link AdoRestService#doGetAdo(String)}.</li>
+     *     <li>If the Ado has a template ID, fetch its template fields via {@link AdoRestService#fetchAdoTemplateFields(Ado)}.</li>
+     *     <li>Log all properties and property values for diagnostic purposes.</li>
+     *     <li>Persist the Ado in the database via {@link AdoDbService#save(Ado)}.</li>
+     * </ol>
+     * Any exception during processing will be wrapped and rethrown as a {@link RuntimeException}.
+     * </p>
+     *
+     * @param eid The entity identifier (EID) of the Ado to process.
+     */
     private void doProcessAdo(String eid) {
         if (synchronizationRegistry.getTransactionStatus() == Status.STATUS_MARKED_ROLLBACK) {
             logger.error("AdoProcessorBean -> Transaction is marked for rollback, skipping");
@@ -68,19 +115,19 @@ public class AdoProcessorBean {
                 adoRestService.fetchAdoTemplateFields(ado);
             }
 
-            for (AdoProperty property : ado.getProperties()) {
-                logger.info("ADO PROPERTIES propertyID = {}, propertyName = {}\n", property.getPropertyId(), property.getPropertyName());
-            }
-
-            for (AdoPropertyValue pv : ado.getPropertyValues()) {
-                logger.info("ADO PROPERTIES VALUES propertyID = {}, propertyValue= {}\n", pv.getPropertyId(), pv.getPropertyValue());
-            }
+            // Only for debugging cases
+//            for (AdoProperty property : ado.getProperties()) {
+//                logger.info("ADO PROPERTIES propertyID = {}, propertyName = {}\n", property.getPropertyId(), property.getPropertyName());
+//            }
+//
+//            for (AdoPropertyValue pv : ado.getPropertyValues()) {
+//                logger.info("ADO PROPERTIES VALUES propertyID = {}, propertyValue= {}\n", pv.getPropertyId(), pv.getPropertyValue());
+//            }
 
             adoDbService.save(ado);
 
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
     }
 }
