b51fe374ed24a01fa5a6d1073f5c529746bae156
Wittchen <swittche@ipb-halle.de>
2025-09-11 11:23:22 +0200

    handling of locations root, ancestor and tray is implemented

--- src/main/java/de/ipb_halle/inhouse/imports/SampleContainerAttacher.java
+++ src/main/java/de/ipb_halle/inhouse/imports/SampleContainerAttacher.java
@@ -57,6 +57,17 @@ public class SampleContainerAttacher {
     public static final String CONTAINER_OBLIGATORY_FIELD_WEIGHT = "739151dc-bd74-407f-8d04-16d4b193a13d";
     public static final String LOCATION_TYPE_ID_TRAY = "e4d8f923-d407-4bee-8121-857a86f9e59d"; //tray
     public static final String LOCATION_ANCESTOR_ROOM_ID_TEST = "2af549e1-e596-44d9-b800-a63e258a58e0"; // room R301.S1
+    private static final String LOCATION_ID_ROOM = "b9fab5b8-6c26-47f8-8694-320c7c439879";
+    // Laborbank ("Bench) e.g. P1
+    private static final String LOCATION_TYPE_ID_P = "81ff1644-2a27-4fb1-a597-5d83e1920c0d";
+    // Schrank ("Cabinet") e.g. S1
+    private static final String LOCATION_TYPE_ID_S = "017929f3-cd0e-466c-ac94-c21ce5fe8c31";
+    // Kühlschrank ("Refrigerator") e.g. K1
+    private static final String LOCATION_TYPE_ID_K = "4211988a-03bf-4103-a8d9-aff5dcf16067";
+    // Kühlschrankfach e.g. sh4 /dr4
+    private static final String LOCATION_TYPE_ID_sh = "3fd2af9d-e066-405c-8a64-d67c4a781e5d";
+    private static final String LOCATION_TYPE_ID_dr = "3fd2af9d-e066-405c-8a64-d67c4a781e5d";
+
 
     private final InhouseDB inhouseDb;
 
@@ -116,8 +127,9 @@ public class SampleContainerAttacher {
                 int num = 1;
                 for (InhouseContainer ic : containers) {
                     // Location
-                    Location locationTray;
+                    Location rootAncestor;
                     Location ancestorLocation;
+                    Location locationTray;
 
                     // find the storage Place e. g TS005
                     String storagePlace = ic.getLocation();
@@ -126,23 +138,58 @@ public class SampleContainerAttacher {
                     String trayPrefix = extractTrayPrefix(storagePlace);
 
                     List<TraysCsvEntry> traysCsvEntries = mapOfStoragePlaceToStorageRoom.get(trayPrefix);
+                    if (traysCsvEntries == null || traysCsvEntries.isEmpty()) {
+                        logger.warn("No CSV entries for tray prefix '{}'", trayPrefix);
+                        continue;
+                    }
                     for (TraysCsvEntry entr : traysCsvEntries) {
                         // e.g. key ="TH001"
                         if (entr.key.equalsIgnoreCase(storagePlace)) {
+                            User user = new User();
+                            user.setId("140");
 
-                            //load ancestor Location e.g. R003.K8                                   R003.K8
-                            ancestorLocation = inhouseDb.getLocationDbService().loadLocationByName(entr.storageRoom);
-                            if (ancestorLocation == null) {
-                                //ancestorLocation = createLocation(locationTypeID, name, description, ancestorLocationId);
+                            //====================== Handling ROOT location ===============================
+                            //load root ancestor location e.g. R003
+                            String roomNumber = entr.storageRoom.trim().split("\\.")[0];
+                            //                                                                  R003
+                            rootAncestor = inhouseDb.getLocationDbService().loadLocationByName(roomNumber);
+                            if (rootAncestor == null) {
+                                String locationTypeId = LOCATION_ID_ROOM;
+                                String name = roomNumber;
+                                String description = String.format("This is a room Nr. %s", roomNumber);
+                                rootAncestor = createLocation(user, locationTypeId, name, description, null, entr);
                             }
 
-                            //                                                                  e.g. TH001
+                            //====================== Handling ANCESTOR location ===============================
+                            //load ancestor Location e.g. R003.K8 or R002.G1 or R2-109.P1                   R003.K8
+                            ancestorLocation = inhouseDb.getLocationDbService().loadLocationByName(entr.storageRoom);
+                            if (ancestorLocation == null) {
+                                String storageDevice = entr.storageRoom.trim().split("\\.")[1];
+                                Matcher m = Pattern.compile("^([A-Za-z]+)(\\d+)$").matcher(storageDevice);
+                                if (!m.find()) {
+                                    logger.error("Cannot parse device type from '{}'", storageDevice);
+                                    continue;
+                                }
+                                String deviceType = m.group(1); // "K", "P", "S", "G", ...
+
+                                String locationTypeID = mapDeviceTypeToLocationTypeId(deviceType);
+                                if (locationTypeID == null) continue;
+                                String name = entr.storageRoom;
+                                String description = String.format("This is a location %s", name);
+
+                                ancestorLocation = createLocation(user, locationTypeID, name, description, rootAncestor.getId(),entr);
+                            }
+
+                            //====================== Handling TRAY location ===============================
+                            // load storage place                                               e.g. TH001
                             locationTray = inhouseDb.getLocationDbService().loadLocationByName(storagePlace);
                             if (locationTray == null) {
                                 logger.info("Location with name = {} is not found in the DB\n", storagePlace);
                                 logger.info("Creating new location\n");
-                                                            // locationTypeID, name, description, ancestorLocationId
-                             //   locationTray = createLocation(LOCATION_TYPE_ID_TRAY, storagePlace, );
+                                String name = storagePlace;
+                                String description = String.format("This is a Tray %s", name);
+                                // locationTypeID, name, description, ancestorLocationId
+                                locationTray = createLocation(user, LOCATION_TYPE_ID_TRAY, name, description, ancestorLocation.getId(), entr);
                             }
 
                             // =========== end of the test code ==============
@@ -152,15 +199,15 @@ public class SampleContainerAttacher {
                             // here the sample eid will be set
                             container.setMaterial(new MaterialReference().setId(eid));
                             // here location eid will be set
-                       //     container.setLocation(new LocationReference().setId(locationEid));
+                            container.setLocation(new LocationReference().setId(locationTray.getId()));
                             container.setContainerTypeId(CONTAINER_TYPE_ID_VIAL);
                             container.setCoordinateX(ic.getRow());
                             container.setCoordinateY(ic.getColumn());
                             container.setAmount(ic.getAmount());
                             container.setUnit(Unit.getUnit("mg"));
-                            container.setDescription("this is a container for sample: " + sample.getName());
-//                            container.setCreatedBy(user);
-//                            container.setUpdatedBy(user);
+                            container.setDescription("This is a container for sample: " + sample.getId());
+                            container.setCreatedBy(user);
+                            container.setUpdatedBy(user);
 
                             List<FieldValue> fieldValues = new ArrayList<>();
 
@@ -192,8 +239,8 @@ public class SampleContainerAttacher {
 
                             ContainerType containerType = new ContainerType();
                             containerType.setId(CONTAINER_TYPE_ID_VIAL);
-                            containerType.setName("Vial " + num++ + " " + System.currentTimeMillis());
-                            containerType.setDescription("This is a vial");
+                            containerType.setName("Vial");
+                            containerType.setDescription("Standard vial");
 
                             String containerEid = inhouseDb.getContainerRestService().doCreateContainer(containerType, container);
                             container.setId(containerEid);
@@ -206,19 +253,35 @@ public class SampleContainerAttacher {
         }
     }
 
-    private Location createLocation(String locationTypeId, String name, String description, String ancestorLocationId) {
+    private String mapDeviceTypeToLocationTypeId(String deviceType) {
+        return switch (deviceType.toUpperCase(Locale.ROOT)) {
+            case "P" -> LOCATION_TYPE_ID_P;   // Bench / P1
+            case "S" -> LOCATION_TYPE_ID_S;   // Cabinet / S1
+            case "K" -> LOCATION_TYPE_ID_K;   // Kühlschrank / K1
+            case "SH" -> LOCATION_TYPE_ID_sh;  // Kühlschrankfach sh*
+            case "DR" -> LOCATION_TYPE_ID_dr;  // Kühlschrankfach dr*
+            default -> {
+                logger.error("Unknown deviceType '{}'", deviceType);
+                yield null;
+            }
+        };
+    }
+
+    private Location createLocation(User user, String locationTypeId, String name, String description, String ancestorLocationId, TraysCsvEntry entry) {
         // toDo: this is a case for Test
         // =========== Create new Location begin of the test code ==============
         Location location = new Location();
         location.setLocationTypeId(locationTypeId);
-        location.setGrid(true);
-        location.setName(String.format("%d_%d", name, System.currentTimeMillis()));
+        location.setName(String.format("%s", name));
         location.setDescription(description);
         location.setAncestorId(ancestorLocationId);
-        User user = new User();
-        user.setId("140");
         location.setCreatedBy(user);
         location.setUpdatedBy(user);
+        if (locationTypeId.equalsIgnoreCase(LOCATION_TYPE_ID_TRAY)) {
+            location.setGrid(true);
+            location.setRows(entry.rows);
+            location.setColumns(entry.columns);
+        }
 
         logger.info("Locations set ={}\n", location.toString());
 
