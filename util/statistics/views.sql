4cb20788ead058331bc689527dc315a727ff45b9
Frank Broda <fbroda@ipb-halle.de>
2026-03-20 12:58:54 +0100

    experiment name statistics

--- util/statistics/views.sql
+++ util/statistics/views.sql
@@ -20,9 +20,25 @@ CREATE VIEW stat_users_by_dept AS
  * Entities by creator, creation date and type
  */
 CREATE VIEW stat_signalsentities_creat AS
- SELECT se.id, se.created_at, de.value AS snb_type,
-   concat(uc.first_name, ' ', uc.last_name) AS created_by
-   FROM signalsentities se
-     JOIN dyn_enums de ON de.id = se.snb_type
-     JOIN users uc ON se.created_by::text = uc.id::text;
+  SELECT se.id, se.created_at, de.value AS snb_type,
+    concat(uc.first_name, ' ', uc.last_name) AS created_by
+    FROM signalsentities se
+      JOIN dyn_enums de ON de.id = se.snb_type
+      JOIN users uc ON se.created_by::text = uc.id::text;
 
+/*
+ * number of objects having non-unique names
+ */
+CREATE VIEW stat_signalsentities_dupnames AS
+  SELECT snb_type, count(snb_type) FROM (
+    SELECT snb_type, count(name), name FROM signalsentities
+    GROUP BY snb_type, name HAVING count(name)>1) AS n
+    GROUP BY snb_type;
+
+/*
+ * number of experiments having non-conforming names
+ */
+CREATE VIEW stat_nonconforming_exp_names AS
+  SELECT count(*) AS nonconforming FROM (
+    SELECT name from signalsentities WHERE snb_type = 1
+    AND name NOT SIMILAR TO '[A-Z]{3,4}[0-9]{3,4}') AS n;
